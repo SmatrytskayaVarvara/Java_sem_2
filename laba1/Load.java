79025a0856c1ff7c363a307a41b263f267035d37
SmatrytskayaVarvara <74321873+SmatrytskayaVarvara@users.noreply.github.com>
2021-12-01 14:51:54 +0100

    Create Load.java

---
+++ laba1/Load.java
@@ -0,0 +1,17 @@
+package laba1;
+
+public class Load {
+    int containersNumber;
+
+    public Load(int containersNumber){
+        this.containersNumber = containersNumber;
+    }
+
+    public int getContainersNumber(){
+        return containersNumber;
+    }
+
+    public void setContainersNumber(int containersNumber){
+        this.containersNumber = containersNumber;
+    }
+}
