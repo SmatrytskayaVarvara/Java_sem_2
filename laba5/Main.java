15b08a6e38ae519bc9249b40dd6840b72f9799a2
SmatrytskayaVarvara <74321873+SmatrytskayaVarvara@users.noreply.github.com>
2021-12-01 15:10:11 +0100

    Create Main.java

---
+++ laba5/Main.java
@@ -0,0 +1,9 @@
+package varia.by;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ContactRunner runner = new ContactRunner();
+        runner.start();
+    }
+}
